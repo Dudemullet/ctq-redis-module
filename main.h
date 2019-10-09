d660be9cb7bfc4443e6a2c38d2e06aca0a273d88
Pipe Gutierrez <felipe.gutierrez@konfio.mx>
2019-10-08 22:21:37 -0500

    Moves constants to main.h file

---
+++ main.h
@@ -0,0 +1,8 @@
+// App level constants
+static const char* CTQ_STORE_NAMESPACE = "ctq:store:";
+static const char* CTQ_TEMP_NAMESPACE = "ctq:temp:";
+static const char* CTQ_STORE_HASH_VALUE = "value";
+static const char* CTQ_STORE_HASH_LIST = "list";
+
+// main.c Methods
+char * appendString(const char* base, const char* stringToAppend);
